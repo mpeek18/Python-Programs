0431c11af831ea94042c2e4525bca1272e1031c9
mpeek18 <33673012+mpeek18@users.noreply.github.com>
2018-06-11 23:13:20 -0400

    Modified Field9_ImageStack 11 June 2018 @2312 hrs

--- Field9_ImageStack.py
+++ Field9_ImageStack.py
@@ -5,6 +5,7 @@ Created on Tue May 29 21:05:05 2018
 @author: Matthew Peek
 Last Modified: 11 June 2018
 """
+import math
 import numpy as np
 from astropy.io import ascii
 import astropy.io.fits as fits
@@ -20,8 +21,46 @@ Algorithm:
         finalImage = numpy 2d image sum
         fits.writeto(image name)   
 """
-#Open Absorber_Data.dat file and get galaxy id's, get image file name, open fits data,
-#use numpy to stack images by summing them together. Write stacked image to new fits file.
+
+"""
+Stack function, takes numpy array as argument, loops through array argument
+getting fits data and combining all image data into same array.
+
+Stack image data standard, mean averaging, and median averaging, compare results.
+Write results to new fits files.
+"""
+def stack(fileList):
+    imageData = [fits.getdata(file) for file in fileList]
+    
+    print ("Total image data:", imageData,'\n')
+    meanImage = np.mean(imageData, axis=0)
+    medianImage = np.median(imageData, axis=0)
+    imageStack = np.sum(imageData, axis=0)
+    
+    fits.writeto('Field9_Stacked_Image_Mean.fits', meanImage, overwrite=True)
+    fits.writeto('Field9_Stacked_Image_Median.fits', medianImage, overwrite=True)
+    fits.writeto('Field9_Stacked_Image.fits', imageStack, overwrite=True)
+    
+    print ("Image Mean:", meanImage,'\n')
+    print ("Image Median:", medianImage,'\n')
+    print ("Image Sum:", imageStack,'\n')
+    
+    plt.clf()
+    plt.imshow(meanImage)
+    plt.colorbar()
+    
+    plt.clf()
+    plt.imshow(medianImage)
+    plt.colorbar()
+    
+    plt.clf()
+    plt.imshow(imageStack)
+    plt.colorbar()
+    print ("Stacking complete!")
+
+
+#Open Absorber_Data.dat file and get galaxy id's, get image file name, open fits data.
+#Start program by reading in id's and appending them to new list.
 count = 0
 absorberFile = ascii.read('Absorption_Data.dat', delimiter='|')
 ID = absorberFile['col2']
@@ -41,20 +80,7 @@ for i in range(1, len(ID)):
         count += 1
     except IOError:
         print ("Image ID " + ID[i] + " not found!")
+print ("Number of images processed:", count,'\n')
 
-print (count) 
-imageData = [fits.getdata(file) for file in fileList]
-
-print ("Total fileList:", imageData)
-finalImage = np.sum(imageData, axis=0)
-fits.writeto('Field9_Stacked_Image.fits', finalImage, overwrite=True)
-
-#plt.imshow(finalImage)
-#plt.colorbar()
-"""
-for x in range(0, len(data)):
-    for y in range(0, len(data[i])):
-        data[x][y] += data[x][y]
-        finalImage = data
-print ("Final", finalImage)
-"""
+#Call Stack function
+stack(fileList) 
