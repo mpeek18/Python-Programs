20ee69a526899df62ea07fc5916226e0b83e0a66
mpeek18 <33673012+mpeek18@users.noreply.github.com>
2018-06-16 17:16:58 -0400

    Modified GalaxySim 16 June 2018 @1716hrs
    Added function to produce random noise in simulated fits images.

--- GalaxySim.py
+++ GalaxySim.py
@@ -3,7 +3,7 @@
 Created on Fri Jun 15 17:45:25 2018
 
 @author: Matthew Peek
-Last Modified: 15 June 2018
+Last Modified: 16 June 2018
 """
 import numpy as np
 import astropy.io.fits as fits
@@ -12,14 +12,32 @@ import astropy.io.fits as fits
 makeCopy function makes copies of given image. Takes numpy array containing
 image data as a paramter and number of copies to make as a parameter
 """
+ID = []
 def makeCopy(imageData, num):
     count = 0
     while (count < num):
-        fits.writeto('Galaxy ' + str(count) + ' .fits', imageData, overwrite=True)
+        fits.writeto('Galaxy_' + str(count) + '.fits', imageData, overwrite=True)
+        ID.append(count)
         count += 1
-    print ("Image copies complete!")
+    print ("Image copies complete!",'\n')
+#End makeCopy function
 
- 
+"""
+addNoise function creates random distribution of noise and adds it to an image.
+Writes new, noisy image, to fits file.
+
+Parameters take a fits image and the current count which image is being
+processed.
+"""
+def addNoise(imageCopy, count):
+    noise = np.random.randn(34, 34)
+    imageNoise = imageCopy + noise
+    fits.writeto('Galaxy_noise_' + str(count) + '.fits', imageNoise, overwrite=True)
+    print ("Adding noise to image complete!", '\n')
+    print ("Noise added to " + str(count + 1) + " images.", '\n')
+#End addNoise function
+    
+################################################################################# 
 """
 Read in single fits image, get image data and call appropriate function.
 Throws error if fits image cannot be found.
@@ -33,5 +51,28 @@ try:
     
     #Call makeCopy function
     makeCopy(imageData, 5)
+    
 except IOError:
     print ("Image not found!")
+    
+#################################################################################    
+"""
+Read in copied galaxy images, pass images to addNoise function.
+Keep count of which image is currently being processed and pass 
+to addNoise function.
+"""
+count = 0
+for i in range(0, len(ID)):
+    try:
+        copyName = 'Galaxy_' + str(ID[i]) + '.fits'
+        file = fits.open(copyName)
+        imageCopy = file[0].data
+        file.close()
+        print (imageCopy,'\n')
+        
+        #Call addNoise function
+        addNoise(imageCopy, count)
+        count += 1
+        
+    except IOError:
+        print ("Galaxy_" + str(ID[i]) + " could not be found!")
